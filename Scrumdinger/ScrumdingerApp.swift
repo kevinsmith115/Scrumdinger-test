31154dc6e43ebd9b65508369d78f1be8cbaeb5b9
Kevin Smith <kevindsmith115@gmail.com>
2023-11-19 02:16:45 -0600

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Kevin Smith on 11/19/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
